9d6cd251cef9b3d500b8a3939a8751cfae0a71c1
Shinku <chenzhuyu@xd.com>
2021-12-01 17:38:30 +0800

    添加罗盘接口

--- api/advertiser/advertisers.go
+++ api/advertiser/advertisers.go
@@ -6,11 +6,11 @@ import (
 )
 
 // AdvertisersGet   获取罗盘绑定广告主列
-func AdvertisersGet(clt *core.SDKClient, accessToken string, advertiserID int64) (*advertiser.Budget, error) {
-	req := &advertiser.BudgetGetRequest{
+func AdvertisersGet(clt *core.SDKClient, accessToken string, advertiserID int64) (*advertiser.AdvertisersResponse, error) {
+	req := &advertiser.AdvertisersRequest{
 		AdvertiserID: advertiserID,
 	}
-	var resp advertiser.Budget
+	var resp advertiser.AdvertisersResponse
 	err := clt.Get(accessToken, req, &resp)
 	if err != nil {
 		return nil, err
