1eac35db80b96580b014595397ce6a5ff09ec675
Shinku <chenzhuyu@xd.com>
2021-12-02 14:21:30 +0800

    获取广告计划信息

--- model/campaign/list_response.go
+++ model/campaign/list_response.go
@@ -2,6 +2,20 @@ package campaign
 
 // ListResponse 获取广告计划信息 API Response
 type ListResponse struct {
-	// CampaignSubType 计划子类型; 4：DPA，5：SDPA
-	CampaignSubType int `json:"campaign_sub_type,omitempty"`
+	TotalCount int       `json:"total_count"`
+	Details    []Details `json:"details"`
+}
+
+type Details struct {
+	Status            int     `json:"status"`
+	CampaignId        int     `json:"campaign_id"`
+	CampaignName      string  `json:"campaign_name"`
+	PutStatus         int     `json:"put_status"`
+	CreateChannel     int     `json:"create_channel"`
+	DayBudget         int64   `json:"day_budget"`
+	DayBudgetSchedule []int64 `json:"day_budget_schedule"`
+	CampaignType      int     `json:"campaign_type"`
+	CampaignSubType   int     `json:"campaign_sub_type"`
+	CreateTime        string  `json:"create_time"`
+	UpdateTime        string  `json:"update_time"`
 }
