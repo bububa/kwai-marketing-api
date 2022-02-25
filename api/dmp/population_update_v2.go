fe8f4ed0c655916bffb45713b558ca8f500fbee4
Shinku <chenzhuyu@xd.com>
2022-02-25 20:19:15 +0800

    添加v2接口

--- api/dmp/population_update_v2.go
+++ api/dmp/population_update_v2.go
@@ -5,7 +5,7 @@ import (
 	"github.com/bububa/kwai-marketing-api/model/dmp"
 )
 
-// PopulationUpdate 人群包更新接口
+// PopulationUpdateV2 人群包更新接口
 func PopulationUpdateV2(clt *core.SDKClient, accessToken string, req *dmp.PopulationUpdateRequestv2) (*dmp.PopulationV2, error) {
 	var resp dmp.PopulationV2
 	err := clt.Post(accessToken, req, &resp)
