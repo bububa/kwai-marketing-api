d066e992eac3afe967daf7dcdb9a36800980e56f
Syd Xu <prof.syd.xu@gmail.com>
2022-06-27 13:10:41 +0800

    refactor(track): add Debug in activate_request

--- api/track/activate.go
+++ api/track/activate.go
@@ -1,9 +1,12 @@
 package track
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/bububa/kwai-marketing-api/core"
@@ -13,6 +16,9 @@ import (
 // Activate 转化回传API
 func Activate(req *track.ActivateRequest) error {
 	reqUrl := fmt.Sprintf("%s?%s", core.ACTIVATE_URL, req.Encode())
+	if req.Debug {
+		log.Println("[DEBUG] [API] GET", reqUrl)
+	}
 	httpReq, err := http.NewRequest("GET", reqUrl, nil)
 	if err != nil {
 		return err
@@ -25,8 +31,21 @@ func Activate(req *track.ActivateRequest) error {
 	var ret struct {
 		Result int `json:"result,omitempty"`
 	}
-	err = json.NewDecoder(httpResp.Body).Decode(&ret)
-	if err != nil {
+	if req.Debug {
+		if body, err := ioutil.ReadAll(httpResp.Body); err != nil {
+			return err
+		} else {
+			body2 := body
+			buf := bytes.NewBuffer(make([]byte, 0, len(body2)+1024))
+			if err := json.Indent(buf, body2, "", "    "); err == nil {
+				body2 = buf.Bytes()
+			}
+			log.Printf("[DEBUG] [API] http response body:\n%s\n", body2)
+			if err := json.Unmarshal(body, &ret); err != nil {
+				return err
+			}
+		}
+	} else if err := json.NewDecoder(httpResp.Body).Decode(&ret); err != nil {
 		return err
 	}
 	if ret.Result != 1 {
