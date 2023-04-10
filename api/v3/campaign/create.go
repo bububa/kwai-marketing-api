38dd386b55fbe2aedc1d2389f7415cbc6e0e2562
michael li <qd_liyuhao@yeah.net>
2023-04-10 14:55:25 +0800

    add v3 campaign

---
+++ api/v3/campaign/create.go
@@ -0,0 +1,11 @@
+package campaign
+
+import (
+	"github.com/bububa/kwai-marketing-api/core"
+	"github.com/bububa/kwai-marketing-api/model/campaign"
+)
+
+// Create 创建广告计划
+func Create(clt *core.SDKClient, accessToken string, req *campaign.CreateRequest) (uint64, error) {
+	return 0, nil
+}
