739519bef8b9cf92dffca2b37a5c399b7a1c0b16
michael li <qd_liyuhao@yeah.net>
2023-05-12 16:51:43 +0800

    updaate

--- api/v2.2/appcenter/app/list.go
+++ api/v2.2/appcenter/app/list.go
@@ -1,5 +1,9 @@
 package app
 
+import (
+	"github.com/bububa/kwai-marketing-api/core"
+	"github.com/bububa/kwai-marketing-api/model/v2.2/appcenter/app"
+)
 // Create 创建创意
 // 【注】联盟广告不支持便利贴图片素材，只有联盟广告支持横版竖版图片素材。
 func List(clt *core.SDKClient, accessToken string, req *app.ListRequest) ([]app.Item, error) {
