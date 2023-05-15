7ed2231e73df35c8d2481b8934971a7fb411b68c
michael li <qd_liyuhao@yeah.net>
2023-05-15 17:52:51 +0800

    dev

--- api/v2.2/appcenter/subpkg/create.go
+++ api/v2.2/appcenter/subpkg/create.go
@@ -5,9 +5,9 @@ import (
 	"github.com/bububa/kwai-marketing-api/model/v2.2/appcenter/subpkg"
 )
 
-func Create(clt *core.SDKClient, accessToken string, req *subpkg.CreateRequest) (subpkg.CreateResponse, error) {
-	var resp subpkg.CreateResponse
-	err := clt.Post(accessToken, req, &resp)
+func Create(clt *core.SDKClient, accessToken string, req *subpkg.CreateRequest) (*subpkg.CreateResponse, error) {
+	var resp *subpkg.CreateResponse
+	err := clt.Post(accessToken, req, resp)
 	if err != nil {
 		return resp, err
 	}
