278eb2257b919615cb12d6ec25a04a11d9fb6d24
michael li <qd_liyuhao@yeah.net>
2023-05-16 18:42:10 +0800

    update omitempty

--- model/v2.2/appcenter/app/list_request.go
+++ model/v2.2/appcenter/app/list_request.go
@@ -5,17 +5,17 @@ type ListRequest struct {
 	//advertiser_id	Long		必填	广告主id
 	AdvertiserId int64 `json:"advertiser_id"`
 	//list_type	Integer		可选	列表类型	不传-全部, 1-我创建的, 2-共享给我的
-	ListType int `json:"list_type,optional"`
+	ListType int `json:"list_type,omitempty"`
 	//platform	String	"ios"	可选	android或ios
-	Platform string `json:"platform,optional"`
+	Platform string `json:"platform,omitempty"`
 	//app_ids	Long[]		可选	批量应用id查询	最多支持查询100个
-	AppIds []int64 `json:"app_ids,optional"`
+	AppIds []int64 `json:"app_ids,omitempty"`
 	//key_word	String		可选	关键词	支持应用ID或应用名称搜索
-	KeyWord string `json:"key_word,optional"`
+	KeyWord string `json:"key_word,omitempty"`
 	//page	Integer		可选	当前页	页码，默认1
-	Page int `json:"page,optional"`
+	Page int `json:"page,omitempty"`
 	//page_size	Integer		可选	分页大小	个数，默认10
-	PageSize int `json:"page_size,optional"`
+	PageSize int `json:"page_size,omitempty"`
 }
 
 func (r ListRequest) Url() string {
