ce179c300b7f49f328fa4249571cf84c04628d66
niewenze <niewenze@xd.com>
2023-06-14 16:19:36 +0800

    fix(native): change url

--- model/native/upload_request.go
+++ model/native/upload_request.go
@@ -6,9 +6,10 @@ import (
 )
 
 type UploadRequest struct {
-	Photo                *model.UploadField `json:"photo"`                  // 视频file
-	ShieldBackwardSwitch bool               `json:"shield_backward_switch"` // 上传视频后是否自动同步至快手个人主页，false表示屏蔽，视频不可在个人主页可见，true表示不屏蔽
-	AuthorID             uint64             `json:"author_id"`              // 原生上传至达人的快手号
+	AdvertiserId         uint64             `json:"advertiser_id"`
+	Photo                *model.UploadField `json:"photo"`                // 视频file
+	ShieldBackwardSwitch bool               `json:"shieldBackwardSwitch"` // 上传视频后是否自动同步至快手个人主页，false表示屏蔽，视频不可在个人主页可见，true表示不屏蔽
+	AuthorID             uint64             `json:"authorId"`             // 原生上传至达人的快手号
 }
 
 // Url implement UploadRequest interface
@@ -28,11 +29,15 @@ func (r UploadRequest) Encode() []model.UploadField {
 	}
 	return []model.UploadField{
 		{
-			Key:   "author_id",
+			Key:   "advertiser_id",
+			Value: strconv.FormatUint(r.AdvertiserId, 10),
+		},
+		{
+			Key:   "authorId",
 			Value: strconv.FormatUint(r.AuthorID, 10),
 		},
 		{
-			Key:   "shield_backward_switch",
+			Key:   "shieldBackwardSwitch",
 			Value: shieldBackwardSwitch,
 		},
 		{
