ef935009cc81530732b6be9d5f36d495430ca24b
michael li <qd_liyuhao@yeah.net>
2023-09-13 22:04:47 +0800

    dev

--- model/v2.2/unit/status_update_request.go
+++ model/v2.2/unit/status_update_request.go
@@ -6,7 +6,7 @@ type StatusUpdateRequest struct {
 	//advertiser_id	long	必填	广告主 ID	在获取 access_token 的时候返回
 	AdvertiserID uint64 `json:"advertiser_id,omitempty"`
 	//unit_ids	long	必填	广告计划 ID
-	UnitIds uint64 `json:"unit_ids"`
+	UnitIds []uint64 `json:"unit_ids"`
 	//1-投放、2-暂停、3-删除，传其他数字非法
 	PutStatus int `json:"put_status"`
 }
