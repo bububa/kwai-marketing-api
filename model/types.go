238ad219a9222c405bd768922556770bf1fac8e3
Syd Xu <prof.syd.xu@gmail.com>
2023-10-10 14:49:46 +0800

    fix(unit): issus #40

---
+++ model/types.go
@@ -0,0 +1,66 @@
+package model
+
+import "strconv"
+
+// Uint64 support string quoted number in json
+type Uint64 uint64
+
+// UnmarshalJSON implement json Unmarshal interface
+func (u64 *Uint64) UnmarshalJSON(b []byte) (err error) {
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+	i, _ := strconv.ParseUint(string(b), 10, 64)
+	*u64 = Uint64(i)
+	return
+}
+
+func (u64 Uint64) Value() uint64 {
+	return uint64(u64)
+}
+
+func (u64 Uint64) String() string {
+	return strconv.FormatUint(uint64(u64), 10)
+}
+
+// Int64 support string quoted number in json
+type Int64 int64
+
+// UnmarshalJSON implement json Unmarshal interface
+func (i64 *Int64) UnmarshalJSON(b []byte) (err error) {
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+	i, _ := strconv.ParseInt(string(b), 10, 64)
+	*i64 = Int64(i)
+	return
+}
+
+func (i64 Int64) Value() int64 {
+	return int64(i64)
+}
+
+func (i64 Int64) String() string {
+	return strconv.FormatInt(int64(i64), 10)
+}
+
+// Float64 support string quoted number in json
+type Float64 float64
+
+// UnmarshalJSON implement json Unmarshal interface
+func (f64 *Float64) UnmarshalJSON(b []byte) (err error) {
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+	i, _ := strconv.ParseFloat(string(b), 64)
+	*f64 = Float64(i)
+	return
+}
+
+func (f64 Float64) Value() float64 {
+	return float64(f64)
+}
+
+func (f64 Float64) String(prec int) string {
+	return strconv.FormatFloat(float64(64), 'f', prec, 64)
+}
