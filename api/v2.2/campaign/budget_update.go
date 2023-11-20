8a221d63e5697189bb12a1bf963f3c380a372b4c
michael li <qd_liyuhao@yeah.net>
2023-11-20 19:21:01 +0800

    add campaign api

--- api/v2.2/campaign/budget_update.go
+++ api/v2.2/campaign/budget_update.go
@@ -3,11 +3,10 @@ package campaign
 import (
 	"github.com/bububa/kwai-marketing-api/core"
 	"github.com/bububa/kwai-marketing-api/model/v2.2/campaign"
-	"github.com/bububa/kwai-marketing-api/model/v2.2/campaign"
 )
 
 // 更新广告组预算
-func BudgetUpdate(clt *core.SDKClient, accessToken string, req *unit.BudgetUpdateRequest) (uint64,error) {
+func BudgetUpdate(clt *core.SDKClient, accessToken string, req *campaign.BudgetUpdateRequest) (uint64,error) {
 	var resp campaign.BudgetUpdateResponse
 	err := clt.Post(accessToken, req, &resp)
 	if err != nil {
--- api/v2.2/campaign/status_update.go
+++ api/v2.2/campaign/status_update.go
@@ -4,11 +4,10 @@ package campaign
 import (
 	"github.com/bububa/kwai-marketing-api/core"
 	"github.com/bububa/kwai-marketing-api/model/v2.2/campaign"
-	"github.com/bububa/kwai-marketing-api/model/v2.2/campaign"
 )
 
 // Create 创建广告组
-func StatusUpdate(clt *core.SDKClient, accessToken string, req *unit.StatusUpdateRequest) ([]uint64, error) {
+func StatusUpdate(clt *core.SDKClient, accessToken string, req *campaign.StatusUpdateRequest) ([]uint64, error) {
 	var resp campaign.StatusUpdateResponse
 	err := clt.Post(accessToken, req, &resp)
 	if err != nil {
