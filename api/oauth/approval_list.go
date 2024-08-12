dc1b3b5a97948d760b5ef03ae0ec83fb293f163f
许彬 <>
2024-08-12 16:49:22 +0800

    fix: 拉取token下授权广告账户接口 页码必填

--- api/oauth/approval_list.go
+++ api/oauth/approval_list.go
@@ -6,11 +6,13 @@ import (
 )
 
 // ApprovalList 拉取token下授权广告账户接口
-func ApprovalList(clt *core.SDKClient, accessToken string) ([]uint64, error) {
+func ApprovalList(clt *core.SDKClient, accessToken string, pageNo int, pageSize int) ([]uint64, error) {
 	req := &oauth.ApprovalListRequest{
 		AppID:       clt.AppID(),
 		Secret:      clt.Secret(),
 		AccessToken: accessToken,
+		PageNo:      pageNo,
+		PageSize:    pageSize,
 	}
 	var resp oauth.ApprovalListResponse
 	err := clt.Post("", req, &resp)
