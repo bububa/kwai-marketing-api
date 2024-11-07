80f93a83f25d01bac20981d03e5342e1e23fbfae
Syd Xu <prof.syd.xu@gmail.com>
2024-11-07 18:53:11 +0800

    fix(oauth): issuse #52

--- model/oauth/access_token.go
+++ model/oauth/access_token.go
@@ -4,12 +4,12 @@ import "encoding/json"
 
 // AccessTokenRequest 获取AccessToken APIRequest
 type AccessTokenRequest struct {
-	// AppID 申请应用后快手返回的 app_id
-	AppID uint64 `json:"app_id,omitempty"`
 	// Secret 申请应用后快手返回的 secret
 	Secret string `json:"secret,omitempty"`
 	// AuthCode 授权时返回的 auth_code
 	AuthCode string `json:"auth_code,omitempty"`
+	// AppID 申请应用后快手返回的 app_id
+	AppID uint64 `json:"app_id,omitempty"`
 }
 
 // Url implement PostRequest interface
@@ -27,14 +27,18 @@ func (r AccessTokenRequest) Encode() []byte {
 type AccessTokenResponse struct {
 	// AccessToken 用于验证权限的token
 	AccessToken string `json:"access_token,omitempty"`
-	// AccessTokenExpiresIn access_token剩余有效时间，单位：秒
-	AccessTokenExpiresIn int64 `json:"access_token_expires_in,omitempty"`
 	// RefreshToken 用于获取新的access_token和refresh_token，并且刷新过期时间
 	RefreshToken string `json:"refresh_token,omitempty"`
+	// AdvertiserIDs 已授权账户所有的account_id
+	AdvertiserIDs []uint64 `json:"advertiser_ids,omitempty"`
+	// AccessTokenExpiresIn access_token剩余有效时间，单位：秒
+	AccessTokenExpiresIn int64 `json:"access_token_expires_in,omitempty"`
 	// RefreshTokenExpiresIn refresh_token剩余有效时间，单位：秒
 	RefreshTokenExpiresIn int64 `json:"refresh_token_expires_in,omitempty"`
 	// AdvertiserID 广告主ID
 	AdvertiserID uint64 `json:"advertiser_id,omitempty"`
-	// AdvertiserIDs 已授权账户所有的account_id
-	AdvertiserIDs []uint64 `json:"advertiser_ids,omitempty"`
+	// UserID
+	UserID uint64 `json:"user_id,omitempty"`
+	// ApproveType
+	ApprovalType int `json:"approval_type,omitempty"`
 }
--- model/oauth/url_request.go
+++ model/oauth/url_request.go
@@ -8,16 +8,16 @@ import (
 
 // UrlRequest 生成授权链接APIRequest
 type UrlRequest struct {
-	// AppID 应用ID
-	AppID uint64 `json:"app_id,omitempty"`
-	// Scope 授权scope
-	Scope []string `json:"scope,omitempty"`
 	// RedirectUri 申请应用时开发者提供的回调地址，使用时需要UrlEncode一次
 	RedirectUri string `json:"redirect_uri,omitempty"`
 	// State 自定义参数; 回调时会原样返回，可用于广告主区分不同投放渠道等用途，广告主可选择性使用
 	State string `json:"state,omitempty"`
 	// OauthType 代理商使用授权URL拼接&oauth_type=agent
 	OauthType string `json:"oauth_type,omitempty"`
+	// Scope 授权scope
+	Scope []string `json:"scope,omitempty"`
+	// AppID 应用ID
+	AppID uint64 `json:"app_id,omitempty"`
 }
 
 // Encode implement GetRequest interface
